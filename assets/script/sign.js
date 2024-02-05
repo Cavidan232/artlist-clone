85c1089e0de0af7863ccaa9fc072765be9ea2e60
cavidan-1907 <cavidanvelizade9@gmail.com>
2024-02-05 17:33:19 +0400

    api

--- assets/script/sign.js
+++ assets/script/sign.js
@@ -1,3 +1,72 @@
+let list2Icon = document.querySelector('.list2');
+let nav2 = document.querySelector('.nav2');
+
+list2Icon.addEventListener("click", () => {
+    if (nav2.style.transform === "translateX(100%)") {
+        nav2.style.transform = "translateX(0)"
+    }
+    else {
+        nav2.style.transform = "translateX(100%)"
+    }
+})
+
+let xicon = document.querySelector(".bi-x");
+xicon.addEventListener("click", () => {
+
+    nav2.style.transform = "translateX(100%)"
+
+
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 991) {
+        nav2.style.transform = "translateX(100%)"
+
+    }
+})
+
+
+
+let list3Icon = document.querySelector('.list1');
+let nav3 = document.querySelector('.nav3');
+
+list3Icon.addEventListener("click", () => {
+    if (nav3.style.transform === "translateX(-100%)") {
+        nav3.style.transform = "translateX(0)"
+    }
+    else {
+        nav3.style.transform = "translateX(-100%)"
+    }
+})
+
+let xicon1 = document.querySelector(".x2");
+xicon1.addEventListener("click", () => {
+
+    nav3.style.transform = "translateX(-100%)"
+
+
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 991) {
+        nav3.style.transform = "translateX(-100%)"
+
+    }
+})
+
+let navLinks = document.querySelectorAll('nav ul li a');
+let nav = document.querySelector("nav");
+
+navLinks.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        nav.style.borderBottom = '2px solid #ffda2a';
+    });
+});
+
+
+
+
+
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector(".btn2");
 const container = document.querySelector(".container");
