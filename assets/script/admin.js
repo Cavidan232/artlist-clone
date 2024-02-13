c173524d885e8a03038242e2fe0467e510b01d6c
cavidan-1907 <cavidanvelizade9@gmail.com>
2024-02-13 16:55:37 +0400

    admin panel commit

--- assets/script/admin.js
+++ assets/script/admin.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", async function() {
-    const tableBody = document.querySelector('.tbod');
+    const tableBody = document.querySelector('.tbody4');
     let allSongs = []; // Tüm şarkıları saklamak için bir dizi
   
     async function fetchAndRenderSongs() {
-      try {
+
         const response = await axios.get('http://localhost:3000/musicData');
         allSongs = response.data;
         renderSongs(allSongs); // Tüm şarkıları tabloya ekle
-      } catch (error) {
-        console.error('xeta:', error);
-      }
+     
     }
   
     // Şarkıları tabloya ekle
@@ -22,7 +20,6 @@ document.addEventListener("DOMContentLoaded", async function() {
             <td>${song.title}</td>
             <td>${song.janre}</td>
             <td>${song.artist}</td>
-            <td><i class="editBtn bi bi-arrow-clockwise" data-id="${song.id}"></i></td>
             <td><i class="deleteBtn bi bi-trash" onclick="deleteCard(${song.id})" data-id="${song.id}"></i></td>
           </tr>
         `;
@@ -34,37 +31,46 @@ document.addEventListener("DOMContentLoaded", async function() {
       renderSongs(filteredSongs); // Filtrelenmiş şarkıları tabloya ekle
     }
   
+    // Arama işlevi
+document.getElementById("search2").addEventListener("input", async (el) => {
+  const searchText = el.target.value.trim().toLowerCase();
+  const response = await axios.get('http://localhost:3000/musicData');
+  const data = await response.data;
+  const filteredData = data.filter((e) => {
+      return e.title.toLowerCase().includes(searchText);
+  });
+  if (filteredData.length === 0) {
+      // Arama sonucunda şarkı bulunamazsa
+      document.querySelector('.tbody4').innerHTML = '<tr><td colspan="5" style="text-align: center;">Axtarışa uyğun nətica tapılmadı ☹</td></tr>';
+  } else {
+      // Arama sonucunda şarkılar bulunursa
+      renderSongs(filteredData);
+  }
+});
+
     // "Pop" türündeki şarkıları göster
     document.querySelector('.pop').addEventListener('click', async function() {
       await filterAndRenderSongs('POP');
     });
-    document.querySelector('.pop1').addEventListener('click', async function() {
-        await filterAndRenderSongs('POP');
-      });
+
   
     // "Hip-Hop" türündeki şarkıları göster
     document.querySelector('.hippop').addEventListener('click', async function() {
       await filterAndRenderSongs('Hip-Hop');
     });
-    document.querySelector('.hippop1').addEventListener('click', async function() {
-        await filterAndRenderSongs('Hip-Hop');
-      });
+
     
     // "Piano" türündeki şarkıları göster
     document.querySelector('.Piano').addEventListener('click', async function() {
       await filterAndRenderSongs('Piano');
     });
-    document.querySelector('.Piano1').addEventListener('click', async function() {
-        await filterAndRenderSongs('Piano');
-      });
+
     
     // Tüm şarkıları göster
     document.querySelector('.All').addEventListener('click', async function() {
       renderSongs(allSongs);
     });
-    document.querySelector('.All1').addEventListener('click', async function() {
-        renderSongs(allSongs);
-      });
+
     
   
     // Sayfa yüklendiğinde tüm şarkıları getir ve tabloya ekle
@@ -76,3 +82,68 @@ document.addEventListener("DOMContentLoaded", async function() {
     window.location.reload();
   }
      
+const html = document.documentElement;
+const body = document.body;
+const menuLinks = document.querySelectorAll(".admin-menu a");
+const collapseBtn = document.querySelector(".admin-menu .collapse-btn");
+const toggleMobileMenu = document.querySelector(".toggle-mob-menu");
+const switchInput = document.querySelector(".switch input");
+const switchLabel = document.querySelector(".switch label");
+const switchLabelText = switchLabel.querySelector("span:last-child");
+const collapsedClass = "collapsed";
+const lightModeClass = "light-mode";
+
+/*TOGGLE HEADER STATE*/
+collapseBtn.addEventListener("click", function () {
+    body.classList.toggle(collapsedClass);
+    this.getAttribute("aria-expanded") == "true"
+        ? this.setAttribute("aria-expanded", "false")
+        : this.setAttribute("aria-expanded", "true");
+    this.getAttribute("aria-label") == "collapse menu"
+        ? this.setAttribute("aria-label", "expand menu")
+        : this.setAttribute("aria-label", "collapse menu");
+});
+
+/*TOGGLE MOBILE MENU*/
+toggleMobileMenu.addEventListener("click", function () {
+    body.classList.toggle("mob-menu-opened");
+    this.getAttribute("aria-expanded") == "true"
+        ? this.setAttribute("aria-expanded", "false")
+        : this.setAttribute("aria-expanded", "true");
+    this.getAttribute("aria-label") == "open menu"
+        ? this.setAttribute("aria-label", "close menu")
+        : this.setAttribute("aria-label", "open menu");
+});
+
+/*SHOW TOOLTIP ON MENU LINK HOVER*/
+for (const link of menuLinks) {
+    link.addEventListener("mouseenter", function () {
+        if (
+            body.classList.contains(collapsedClass) &&
+            window.matchMedia("(min-width: 768px)").matches
+        ) {
+            const tooltip = this.querySelector("span").textContent;
+            this.setAttribute("title", tooltip);
+        } else {
+            this.removeAttribute("title");
+        }
+    });
+}
+
+/*TOGGLE LIGHT/DARK MODE*/
+if (localStorage.getItem("dark-mode") === "false") {
+    html.classList.add(lightModeClass);
+    switchInput.checked = false;
+    switchLabelText.textContent = "Light";
+}
+
+switchInput.addEventListener("input", function () {
+    html.classList.toggle(lightModeClass);
+    if (html.classList.contains(lightModeClass)) {
+        switchLabelText.textContent = "Light";
+        localStorage.setItem("dark-mode", "false");
+    } else {
+        switchLabelText.textContent = "Dark";
+        localStorage.setItem("dark-mode", "true");
+    }
+});
